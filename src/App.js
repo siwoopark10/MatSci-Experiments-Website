0e28bb4597f812af0aed7b12240131ee91c9be58
Siwoo <siwoo2000@gmail.com>
2021-01-31 18:25:51 -0600

    app bar from materials-ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./styles.css";
+import MenuAppBar from "./components/MenuAppBar";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Argonne - Material Science Experiments</h1>
+    <div>
+      <MenuAppBar />
+      <div className="App">
+      </div>
     </div>
   );
 }
