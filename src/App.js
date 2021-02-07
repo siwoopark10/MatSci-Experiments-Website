3093aebd77d3a18ad304b2575292728a25d8f9ce
lilyyan2023 <56418644+lilyyan2023@users.noreply.github.com>
2021-02-06 20:48:39 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./styles.css";
 import MenuAppBar from "./components/MenuAppBar";
 import NavTabs from "./components/NavTabs";
+import ParameterTextBox from "./components/ParameterTextBox";
 
 export default function App() {
   return (
     <div>
       <MenuAppBar />
       <div className="App">
+      
       </div>
       <NavTabs />
+      <ParameterTextBox/>
+      <Slider />
     </div>
   );
 }
