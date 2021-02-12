9095be6bd63f00ada99acbc658741894f164fdb3
PrabhavJain10 <67673958+PrabhavJain10@users.noreply.github.com>
2021-02-12 17:19:07 -0600

    React-Router-Dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
+
+
 ReactDOM.render(
-  <StrictMode>
+  <Router>
     <App />
-  </StrictMode>,
+  </Router>,
   rootElement
 );
